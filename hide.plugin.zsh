a923e2f261ff890cdfe487b2226748eba1a36dcf
Aubrey Portwood <aubreypwd@users.noreply.github.com>
2020-10-01 11:41:35 -0600

    Update hide.plugin.zsh

--- hide.plugin.zsh
+++ hide.plugin.zsh
@@ -1,15 +1,4 @@
 #!/bin/zsh
 
-###
- # My Plugin
- #
- # @since 1.0.0
- ##
-
-function hide {
- chflags hidden "$1"
-}
-
-function unhide {
- chflags nohidden "$1"
-}
+alias hide="chflags hidden"
+alias unhide="chflags nohidden"
